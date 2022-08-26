7083f680ee196ff5bde8cd9a60bd669e4e91f39d
Chris Cromer <chris@cromer.cl>
2022-08-26 13:48:39 -0400

    ignore delta since we don't use it

--- godot/hud/CoinHUD.gd
+++ godot/hud/CoinHUD.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	if get_tree().paused:
 		visible = false
 	else:

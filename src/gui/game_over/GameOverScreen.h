ff600cd7614ac74d3b72be7f4ad2a11a7852135a
Chris Cromer <chris@cromer.cl>
2022-08-26 13:48:59 -0400

    fix spacing

--- src/gui/game_over/GameOverScreen.h
+++ src/gui/game_over/GameOverScreen.h
@@ -8,51 +8,49 @@
 
 namespace godot
 {
-        /**
-         * @brief This class controls what happens when the Coin is in the collected .
-         * 
-         */
-        class GameOverScreen : public Control
-        {
-            GODOT_CLASS(GameOverScreen, Control)
+	/**
+	 * @brief This class controls what happens when the Coin is in the collected .
+	 * 
+	 */
+	class GameOverScreen : public Control
+	{
+		GODOT_CLASS(GameOverScreen, Control)
 
-            private:
+		private:
 
+		public:
+		/**
+		 * @brief This method registers classes with Godot.
+		 * 
+		 * @details This method registers methods, properties, and signals with the Godot engine.
+		 */
+		static void _register_methods();
 
-            public:
-                /**
-                 * @brief This method registers classes with Godot.
-                 * 
-                 * @details This method registers methods, properties, and signals with the Godot engine.
-                 */
-                static void _register_methods();
+		/**
+		 * @brief Construct a new GameOverScreen object.
+		 * 
+		 */
+		GameOverScreen();
 
-                /**
-                 * @brief Construct a new GameOverScreen object.
-                 * 
-                 */
-                GameOverScreen();
+		/**
+		 * @brief Destroy the GameOverScreen object.
+		 * 
+		 */
+		~GameOverScreen();
 
-                /**
-                 * @brief Destroy the GameOverScreen object.
-                 * 
-                 */
-                ~GameOverScreen();
+		/**
+		 * @brief Initialize the class from Godot.
+		 * 
+		 * @details This method is called just once when the Godot engine connects to the instance of the class.
+		 */
+		void _init();
 
-                /**
-                 * @brief Initialize the class from Godot.
-                 * 
-                 * @details This method is called just once when the Godot engine connects to the instance of the class.
-                 */
-                void _init();
-
-                /**
-                 * @brief Called when the collected  of the coin is entered.
-                 * 
-                 */
-                void _on_botonreiniciar_pressed();
-        };
-    
+		/**
+		 * @brief Called when the collected  of the coin is entered.
+		 * 
+		 */
+		void _on_botonreiniciar_pressed();
+	};
 }
 
 #endif
